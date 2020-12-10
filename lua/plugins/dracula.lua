375fae90d972cd6f4e56e3f8cea12c294edd6d15
evakuator <kuatabylkasymov@gmail.com>
2020-12-10 17:08:02 +0600

    remove redundant files

--- lua/plugins/dracula.lua
+++
@@ -1,3 +0,0 @@
-return function ()
-  vim.cmd('colorscheme dracula')
-end
--- lua/plugins/gruvbox.lua
+++
@@ -1,4 +0,0 @@
-return function ()
-    -- require("colorbuddy").colorscheme("gruvbox")
-    vim.cmd('colorscheme gruvbox')
-end
