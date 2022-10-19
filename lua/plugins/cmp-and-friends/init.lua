7358050c320197a0e7898902d97a404344f73b08
evakuator <kuatabylkasymov@gmail.com>
2022-10-19 12:09:59 +0600

    update luasnip

--- lua/plugins/cmp-and-friends/init.lua
+++ lua/plugins/cmp-and-friends/init.lua
@@ -8,22 +8,7 @@ return {
     requires = {
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-nvim-lsp",
-      {
-        "L3MON4D3/LuaSnip",
-        -- wants = { "friendly-snippets" },
-        -- wants = { "vim-snippets" },
-        -- requires = {
-          -- "rafamadriz/friendly-snippets",
-          -- "honza/vim-snippets",
-        -- },
-        opt = true,
-        config = function ()
-          -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/friendly-snippets" } })
-          require("luasnip.loaders.from_vscode").lazy_load({ paths = { './snippets' } })
-          -- require("luasnip.loaders.from_snipmate").lazy_load()
-          -- require("luasnip.loaders.from_vscode").lazy_load()
-        end
-      }
+      require "plugins.cmp-and-friends.luasnip-nvim",
     },
   },
   require('plugins.cmp-and-friends.skkeleton')
---
+++ lua/plugins/cmp-and-friends/luasnip-nvim.lua
@@ -0,0 +1,73 @@
+return {
+  "L3MON4D3/LuaSnip",
+  -- wants = { "friendly-snippets" },
+  -- wants = { "vim-snippets" },
+  -- requires = {
+  -- "rafamadriz/friendly-snippets",
+  -- "honza/vim-snippets",
+  -- },
+  opt = true,
+  config = function()
+    -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/friendly-snippets" } })
+    local luasnip = require 'luasnip'
+    local node_util = require("luasnip.nodes.util")
+    luasnip.config.setup {
+      parser_nested_assembler = function(_, snippet)
+        local select = function(snip, no_move)
+          snip.parent:enter_node(snip.indx)
+          -- upon deletion, extmarks of inner nodes should shift to end of
+          -- placeholder-text.
+          for _, node in ipairs(snip.nodes) do
+            node:set_mark_rgrav(true, true)
+          end
+
+          -- SELECT all text inside the snippet.
+          if not no_move then
+            vim.api.nvim_feedkeys(
+              vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
+              "n",
+              true
+            )
+            node_util.select_node(snip)
+          end
+        end
+        function snippet:jump_into(dir, no_move)
+          if self.active then
+            -- inside snippet, but not selected.
+            if dir == 1 then
+              self:input_leave()
+              return self.next:jump_into(dir, no_move)
+            else
+              select(self, no_move)
+              return self
+            end
+          else
+            -- jumping in from outside snippet.
+            self:input_enter()
+            if dir == 1 then
+              select(self, no_move)
+              return self
+            else
+              return self.inner_last:jump_into(dir, no_move)
+            end
+          end
+        end
+
+        -- this is called only if the snippet is currently selected.
+        function snippet:jump_from(dir, no_move)
+          if dir == 1 then
+            return self.inner_first:jump_into(dir, no_move)
+          else
+            self:input_leave()
+            return self.prev:jump_into(dir, no_move)
+          end
+        end
+
+        return snippet
+      end
+    }
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { './snippets' } })
+    -- require("luasnip.loaders.from_snipmate").lazy_load()
+    -- require("luasnip.loaders.from_vscode").lazy_load()
+  end
+}
