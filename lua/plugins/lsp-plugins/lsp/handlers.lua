a8b889175bac299c9698963f98fb64b6637bf3e1
evakuator <kuatabylkasymov@gmail.com>
2022-10-29 21:56:03 +0600

    update

--- lua/plugins/lsp-plugins/lsp/handlers.lua
+++ lua/plugins/lsp-plugins/lsp/handlers.lua
@@ -1,31 +1,38 @@
 local M = {}
 
 local function lsp_keymaps(client, bufnr)
+
   -- Keybindings for LSPs
-  vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>ge", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
-  -- vim.api.nvim_set_keymap("n", "<leader>gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
-  vim.api.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gt", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>gw", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<a-.>", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+  local options = { noremap = true, silent = true }
+  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, options)
+  vim.keymap.set("n", "<leader>ge", vim.lsp.buf.declaration, options)
+  vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, options)
+  vim.keymap.set('n', '<space>gf', function() vim.lsp.buf.format { async = false } end, options)
+  vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, options)
+  -- vim.keymap.set("n", "<leader>gk",  vim.lsp.buf.signature_help, options)
+  vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, options)
+  vim.keymap.set("n", "<leader>gy", vim.lsp.buf.type_definition, options)
+  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, options)
+  vim.keymap.set("n", "<leader>gt", vim.lsp.buf.document_symbol, options)
+  vim.keymap.set("n", "<leader>gw", vim.lsp.buf.workspace_symbol, options)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, options)
+  vim.keymap.set("n", "<a-.>", vim.lsp.buf.code_action, options)
+
   -- vim.api.nvim_command('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 
   -- https://github.com/MurdeRM3L0DY/dotfiles/blob/b756d26a39351366ab30086440e1fbe8655efc39/roles/nvim/files/lua/plugins/null-ls/config.lua
   if client.supports_method 'textDocument/formatting' then
-    vim.keymap.set('n', '<leader>F', function()
-      vim.lsp.buf.format({
-        filter = function(client) return client.name ~= "tsserver" end,
-        bufnr = bufnr,
-      })
-    end,
-      { buffer = bufnr })
+    vim.keymap.set(
+      'n',
+      '<leader>F',
+      function()
+        vim.lsp.buf.format({
+          filter = function(client) return client.name ~= "tsserver" end,
+          bufnr = bufnr,
+        })
+      end,
+      { buffer = bufnr }
+    )
   end
 
   -- if client.supports_method 'textDocument/rangeFormatting' then
@@ -93,10 +100,10 @@ M.on_attach = function(client, bufnr)
     )
   end
 
-  local status_ok, aerial = pcall(require, "aerial")
-  if status_ok then
-    aerial.on_attach(client, bufnr)
-  end
+  -- local status_ok, aerial = pcall(require, "aerial")
+  -- if status_ok then
+  --   aerial.on_attach(client, bufnr)
+  -- end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
