fbbaea95ef70d2453785cd0cf3caa3be83692398
evakuator <kuatabylkasymov@gmail.com>
2023-05-20 14:52:13 +0600

    feat: add pyink range formatting

--- lua/plugins/null_ls/init.lua
+++ lua/plugins/null_ls/init.lua
@@ -4,10 +4,39 @@ local function config()
     return
   end
 
+  local h = require("null-ls.helpers")
+  local methods = require("null-ls.methods")
+
+  local FORMATTING = methods.internal.FORMATTING
+  local RANGE_FORMATTING = methods.internal.RANGE_FORMATTING
+
+
+  local pyink = h.make_builtin({
+      name = "pyink",
+      meta = {
+          url = "https://github.com/google/pyink",
+          description = "The Google Python code formatter",
+      },
+      method = {FORMATTING, RANGE_FORMATTING},
+      filetypes = { "python" },
+      generator_opts = {
+          command = "pyink",
+          args = h.range_formatting_args_factory({
+              "--stdin-filename",
+              "$FILENAME",
+              "--quiet",
+              "-",
+          }, "--pyink-lines", nil, { use_rows = true, delimiter = "-" }),
+          to_stdin = true,
+      },
+      factory = h.formatter_factory,
+  })
+
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
   local formatting = null_ls.builtins.formatting
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   local diagnostics = null_ls.builtins.diagnostics
+
 
   null_ls.setup({
     debug = true,
@@ -15,7 +44,8 @@ local function config()
     sources = {
       formatting.stylua.with({ extra_args = { "--indent_type", "Spaces", "indent_width", "2" } }),
       diagnostics.ruff,
-      formatting.pyink,
+      -- formatting.pyink,
+      pyink,
       formatting.jq,
     },
   })
