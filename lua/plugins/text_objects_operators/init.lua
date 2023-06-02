7b8e1d2ce6042ef187c4f017e53fd2d82931bc3d
evakuator <kuatabylkasymov@gmail.com>
2023-06-02 16:34:26 +0600

    update

--- lua/plugins/text_objects_operators/init.lua
+++ lua/plugins/text_objects_operators/init.lua
@@ -80,6 +80,19 @@ return {
     lazy = true,
     keys = { { mode = 'n', 'gs' }, { mode = 'x', 'gs' }, },
   },
+  {
+    "andymass/vim-matchup",
+    event = "VeryLazy",
+    init = function()
+      vim.g.matchup_surround_enabled = 1
+      vim.g.matchup_matchparen_nomode = "i"
+      vim.g.matchup_matchparen_deferred = 1
+      vim.g.matchup_matchparen_deferred_show_delay = 400
+      vim.g.matchup_matchparen_deferred_hide_delay = 400
+      -- vim.g.matchup_matchparen_offscreen = {}
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  }
   -- 'tommcdo/vim-exchange'
   -- {
   --   'ckolkey/ts-node-action',
