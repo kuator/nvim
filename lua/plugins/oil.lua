d0ad9cebae89ee58b98cf1e0d0d60509db339230
evakuator <>
2023-11-12 14:51:17 +0600

    update

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  config = function ()
+    require("oil").setup()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+  keys = {
+    { mode = 'n', '-' },
+  },
+}
