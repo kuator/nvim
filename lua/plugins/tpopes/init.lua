dce41b71457ffea8dbfbb3a885aba21854835edf
evakuator <>
2023-11-25 21:24:38 +0600

    update

--- lua/plugins/tpopes/init.lua
+++ lua/plugins/tpopes/init.lua
@@ -1,19 +1,30 @@
 return {
   -- {
   --   'tpope/vim-commentary';
-    -- keys = { { mode='x'; 'gc' }; { mode='n'; 'gc' } };
+  -- keys = { { mode='x'; 'gc' }; { mode='n'; 'gc' } };
   -- },
+
+  -- {
+  --   'tpope/vim-surround';
+  --   keys = { { mode='n'; 'ys' }; { mode='x'; 'S' }; { mode='n'; 'cs' }; { mode='n'; 'ds' }; };
+  -- },
+
   {
-    'tpope/vim-surround';
-    keys = { { mode='n'; 'ys' }; { mode='x'; 'S' }; { mode='n'; 'cs' }; { mode='n'; 'ds' }; };
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    keys = { { mode = 'n', 'ys' }, { mode = 'x', 'S' }, { mode = 'n', 'cs' }, { mode = 'n', 'ds' }, },
+    config = function()
+      require "nvim-surround".setup {}
+    end
+  },
+
+  {
+    'tpope/vim-unimpaired',
+    keys = { { mode = 'n', '[p' }, { mode = 'n', ']p' }, { mode = 'n', '[l' }, { mode = 'n', ']l' }, }
   },
   {
-    'tpope/vim-unimpaired';
-    keys = { { mode='n'; '[p' }; { mode='n'; ']p' }; { mode='n'; '[l' }; { mode='n'; ']l' }; }
-  },
-  {
-    'tpope/vim-repeat';
-    keys = { { mode='n'; '.' } };
+    'tpope/vim-repeat',
+    keys = { { mode = 'n', '.' } },
   },
   {
     'tpope/vim-rsi',
